19164376c9f9b2fd8f68ce7603bf737508f7b86c
Jens Kulka <kulkajens82@gmail.com>
2023-10-18 13:12:53 +0200

    Feature: Added sighandler so ctrl-c doesn't exit the shell

--- src/main.c
+++ src/main.c
@@ -6,18 +6,30 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:16:36 by jkulka            #+#    #+#             */
-/*   Updated: 2023/10/17 13:07:17 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/10/18 12:43:03 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+char *home_dir;
+void handlectrl(int signum)
+{
+    (void)signum;
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+    ft_printf(BLU"\n>> "WHT);
+}
 int main()
 {
     char *str;
     char **arg;
-    home_dir = getenv("HOME");
-    while((str = readline(">> ")))
+    home_dir = getenv("PATH");
+    signal(SIGINT, handlectrl);
+    while((str = readline(BLU">> "WHT)))
     {
+
         str[strcspn(str, "\n")] = '\0';
         if(str[0] == '\0')
         {
@@ -25,6 +37,8 @@ int main()
             continue;
         }
         arg = ft_new_split(str);
+        if(str[0] != '\0')
+            add_history(str);
         ft_wait_for_cmd(arg);
     }
 }
