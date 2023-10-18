5c68bc05ef4189ec6db86792ad90bdaea4e8182f
Jens Kulka <kulkajens82@gmail.com>
2023-10-18 13:12:23 +0200

    Chore: Added prototypes to header and better global variable

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 13:15:40 by jkulka            #+#    #+#             */
-/*   Updated: 2023/10/17 13:13:50 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/10/18 13:08:51 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,25 @@
 #include "../Libft/src/libft.h"
 #include <stdio.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 #include <dirent.h>
 #include <limits.h>
 #include <string.h>
+#include <fcntl.h>
+
 #	define RED "\e[0;31m"
 #	define YEL "\e[0;33m"
 #	define WHT "\e[0;37m"
 #	define BLU "\e[0;34m"
 /* Global Variable */
-char *home_dir;
+extern char *home_dir;
 /* Commands */
-int list_files();
+// int list_files(char *path);
+void ft_echo(char **arg);
 void clear();
 void ft_wait_for_cmd(char **arg);
 int execute_command(char **arg);
+// int execute_with_output_redirection(const char *command, const char *output_file);
 /* String Utils */
 char *ft_last_word(char *str);
 char *first_word(char *str);
