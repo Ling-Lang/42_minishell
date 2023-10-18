38174e9d175907bff7e9ab54bb4f0d184946cb06
Jens Kulka <kulkajens82@gmail.com>
2023-10-18 13:13:26 +0200

    Chore: Removed ls because it is not needed for project

--- src/utils/cmd.c
+++ src/utils/cmd.c
@@ -6,29 +6,11 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 12:05:35 by jkulka            #+#    #+#             */
-/*   Updated: 2023/10/17 13:23:12 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/10/18 13:05:53 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-int list_files()
-{
-    struct dirent *entry;
-    DIR *dp = opendir("."); // Open the current directory
-
-    if (dp == NULL) {
-        perror("opendir");
-        return -1;
-    }
-
-    while ((entry = readdir(dp)) != NULL) {
-        ft_printf("%s\n", entry->d_name);
-    }
-
-    closedir(dp);
-    return 0;
-}
 
 void clear()
 {
