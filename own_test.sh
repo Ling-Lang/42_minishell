e7a97b3fe09f29d7a170ebc61e262837a0367473
Jens Kulka <kulkajens82@gmail.com>
2023-11-20 18:26:38 +0100

    WIP: Almost finished fixing env variables.
    BROKEN: 130 and echo  doesn't work for now will fix tomorrow
    FEATURE: Lexer expands now multiple variables in the same string.
    BROKEN: Variables don't work without double quotes for now

---
+++ own_test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+MPATH="./minishell"
+PROMPT=$(echo -e "\nexit\n" | $MPATH | head -n 1 | sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g" )
+REMOVE_COLORS="sed s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g"
+REMOVE_EXIT="grep -v ^exit$"
+
+mkdir ./mout
+mkdir ./bout
+mkdir ./out
+function exec_test()
+{
+	TEST1=$(echo -e $@ | ./minishell 2>&- /dev/null | $REMOVE_COLORS | grep -vF "$PROMPT" | $REMOVE_EXIT )
+    # TEST1=$(echo $@ "; exit" | ./minishell 2>&-)
+    ES_1="$?"
+    TEST2=$(echo -e $@ | bash 2>&-)
+	ES_2=$?
+    # printf "\n$MINI_OUTPUT\n"
+    if [ "$TEST1" == "$TEST2" ] && [ "$ES_1" == "$ES_2" ]; then
+		printf "✓ "
+    else
+        printf "$ES_1\t$ES_2\n$TEST1\t$TEST2\n"
+    fi
+    echo
+    sleep 0.1
+}
+function exec_diff_test()
+{
+    rm -rf ./out/*
+    rm -rf ./mout/*
+    TEST1=$(echo -e $@ | ./minishell 2>&- /dev/null | $REMOVE_COLORS | grep -vF "$PROMPT" | $REMOVE_EXIT )
+    MOUT=$(cp ./out/* ./mout &>/dev/null)
+    ES_1="$?"
+    rm -rf ./out/*
+    rm -rf ./bout/*
+    TEST2=$(echo -e $@ | bash 2>&-)
+    BOUT=$(cp ./out/* ./bout &>/dev/null)
+	ES_2=$?
+    ODIFF=$(diff --brief ./mout ./bout)
+    if [ "$TEST1" == "$TEST2" ] && [ "$ES_1" == "$ES_2" ] && [ -z "$ODIFF" ]; then
+		printf "✓ "
+    else
+        printf "$ES_1\t$ES_2\n$TEST1\t$TEST2\n"
+    fi
+
+}
+TEST_FILE="file"
+# exec_test 'echo test'
+exec_test 'pwd'
+exec_test 'echo "$PWD"'
+# exec_diff_test 'echo test >> file'
+exec_test 'ls'
+
+rm -rf ./out
+rm -rf ./bout
+rm -rf ./mout
