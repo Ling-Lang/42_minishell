e289f79a3799d54e9f8ba1718c7ecf046968f522
Jens Kulka <kulkajens82@gmail.com>
2024-02-16 13:25:58 +0100

    Intepreter function splitting

--- src/interpreter/i_cmd.c
+++ src/interpreter/i_cmd.c
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de >    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:53:40 by jkulka            #+#    #+#             */
-/*   Updated: 2024/02/05 12:05:36 by jkulka           ###   ########.fr       */
+/*   Updated: 2024/02/16 11:11:33 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,30 +104,71 @@ int	ft_check_heredoc(t_node *tree, int *stop, int **fd)
 	return (0);
 }
 
-void	exec_tree(t_node *tree, t_env **env, t_return *ret)
-{
-	int	n;
-	int	fd[2];
-	int	*here_fd;
-	int	stop;
+// void	exec_tree(t_node *tree, t_env **env, t_return *ret)
+// {
+// 	int	n;
+// 	int	fd[2];
+// 	int	*here_fd;
+// 	int	stop;
 
-	stop = 0;
-	fd[0] = -1;
-	ret->r_code = 1;
-	here_fd = NULL;
-	if (ft_check_heredoc(tree, &stop, &here_fd) == ERR)
-		return ;
-	if (!stop)
-	{
-		cache_fd((int *)fd);
-		n = find_symbol(tree, A_PIPE, 0);
-		if (handle_redirects(tree, 0, here_fd) != ERR)
-		{
-			if (n)
-				ret->r_code = simple_command2(tree, fd, env);
-			else
-				simple_command(tree, fd, env, ret);
-		}
-	}
-	clean_here(here_fd);
+// 	stop = 0;
+// 	fd[0] = -1;
+// 	ret->r_code = 1;
+// 	here_fd = NULL;
+// 	if (ft_check_heredoc(tree, &stop, &here_fd) == ERR)
+// 		return ;
+// 	if (!stop)
+// 	{
+// 		cache_fd((int *)fd);
+// 		n = find_symbol(tree, A_PIPE, 0);
+// 		if (handle_redirects(tree, 0, here_fd) != ERR)
+// 		{
+// 			if (n)
+// 				ret->r_code = simple_command2(tree, fd, env);
+// 			else
+// 				simple_command(tree, fd, env, ret);
+// 		}
+// 	}
+// 	clean_here(here_fd);
+// }
+
+void initialize_exec_tree(int *stop, int *fd, int **here_fd, t_return *ret)
+{
+    *stop = 0;
+    fd[0] = -1;
+    ret->r_code = 1;
+    *here_fd = NULL;
+}
+
+void handle_commands(t_node *tree, int *fd, t_env **env, t_return *ret, int *here_fd)
+{
+    int n = find_symbol(tree, A_PIPE, 0);
+    if (handle_redirects(tree, 0, here_fd) != ERR)
+    {
+        if (n)
+            ret->r_code = simple_command2(tree, fd, env);
+        else
+            simple_command(tree, fd, env, ret);
+    }
+}
+
+void exec_tree(t_node *tree, t_env **env, t_return *ret)
+{
+    // int n;
+    int fd[2];
+    int *here_fd;
+    int stop;
+
+    initialize_exec_tree(&stop, fd, &here_fd, ret);
+
+    if (ft_check_heredoc(tree, &stop, &here_fd) == ERR)
+        return ;
+
+    if (!stop)
+    {
+        cache_fd((int *)fd);
+        handle_commands(tree, fd, env, ret, here_fd);
+    }
+
+    clean_here(here_fd);
 }
